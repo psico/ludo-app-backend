c9ca228222477281ada71cb5922b1e5832674a43
psico <desenvolvedorjg@gmail.com>
2021-01-07 11:58:13 -0300

    [ADD] First try to user lib express-graphql

---
+++ index.ts
@@ -0,0 +1,61 @@
+const app = require("express")();
+const expressGraphql = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+const schema = buildSchema(`
+  type User {
+    id: ID
+    name: String
+    repo: String
+    age: Int
+  }
+  type Query {
+    user(id: ID!): User
+    users: [User]
+  }
+  type Mutation {
+    createUser(name: String!, repo: String!, age: Int!): User
+  }
+`);
+
+const providers = {
+    users: []
+};
+
+let id = 0;
+
+const resolvers = {
+    // @ts-ignore
+    user({ id }) {
+        // @ts-ignore
+        return providers.users.find(item => item.id === Number(id));
+    },
+    users() {
+        return providers.users;
+    },
+    // @ts-ignore
+    createUser({ name, repo, age }) {
+        const user = {
+            id: id++,
+            name,
+            repo,
+            age
+        };
+
+        // @ts-ignore
+        providers.users.push(user);
+
+        return user;
+    }
+};
+
+app.use(
+    "/graphql",
+    expressGraphql({
+        schema,
+        rootValue: resolvers,
+        graphiql: true
+    })
+);
+
+app.listen(4000);
