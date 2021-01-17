6236affcc7bb061492e3a0080f45d6ad2d2cdfbe
psico <desenvolvedorjg@gmail.com>
2021-01-17 15:26:12 -0300

    [ADD] Started to separe resolver

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,25 @@ const path = require("path");
 const schemaFile = path.join(__dirname, "schema.graphql");
 const typeDefs = fs.readFileSync(schemaFile, "utf8");
 
-const schema = makeExecutableSchema({ typeDefs });
+const resolvers = {
+    Query: {
+        Users: () => {
+            return providers.users;
+        },
+        User: (_, { id }) => {
+            // @ts-ignore
+            return providers.users.find(item => item.id === Number(id));
+        },
+    },
+    User: {
+        id: user => user.id,
+        name: user => user.name,
+        repo: user => user.repo,
+        age: user => user.age,
+    },
+};
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 const providers = {
     users: []
@@ -15,38 +33,46 @@ const providers = {
 
 let id = 0;
 
-const resolvers = {
-    // @ts-ignore
-    user({ id }) {
-        // @ts-ignore
-        return providers.users.find(item => item.id === Number(id));
-    },
-    users() {
-        return providers.users;
-    },
-    // @ts-ignore
-    createUser({ name, repo, age }) {
-        const user = {
-            id: id++,
-            name,
-            repo,
-            age
-        };
-
-        // @ts-ignore
-        providers.users.push(user);
-
-        return user;
-    }
-};
+// const resolvers = {
+//     // @ts-ignore
+//     user({ id }) {
+//         // @ts-ignore
+//         return providers.users.find(item => item.id === Number(id));
+//     },
+//     users() {
+//         return providers.users;
+//     },
+//     // @ts-ignore
+//     createUser({ name, repo, age }) {
+//         const user = {
+//             id: id++,
+//             name,
+//             repo,
+//             age
+//         };
+//
+//         // @ts-ignore
+//         providers.users.push(user);
+//
+//         return user;
+//     }
+// };
 
 var app = express();
+
+// app.use(
+//     "/graphql",
+//     graphqlHTTP({
+//         schema,
+//         rootValue: resolvers,
+//         graphiql: true
+//     })
+// );
 
 app.use(
     "/graphql",
     graphqlHTTP({
         schema,
-        rootValue: resolvers,
         graphiql: true
     })
 );
