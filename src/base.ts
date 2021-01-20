596a249945e7116f275875e7b797bf2aa2c67ebf
psico <desenvolvedorjg@gmail.com>
2021-01-19 21:27:08 -0300

    [ADD] Base schema

---
+++ src/base.ts
@@ -0,0 +1,15 @@
+const Base = `
+type Query {
+    dummy: Boolean
+}
+type Mutation {
+    dummy: Boolean
+}
+type Meta {
+    count: Int
+}
+scalar Url
+scalar Date
+`;
+
+export default () => [Base];
