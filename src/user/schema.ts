a7b19d3ee0ce78201e3460c48ac3bdbe08e87217
psico <desenvolvedorjg@gmail.com>
2021-01-19 21:07:46 -0300

    [ADD] User schema implemented

---
+++ src/user/schema.ts
@@ -0,0 +1,15 @@
+const Tweet = `
+extend type Query {
+    user(id: ID!): User
+    users: [User]
+}
+extend type Mutation {
+    createUser(name: String!, repo: String!, age: Int!): User
+}
+type User {
+    id: ID
+    name: String
+    repo: String
+    age: Int
+}
+`;
