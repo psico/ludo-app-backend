5cb6c88b60ba4f2c37d5bc310c52139d51948962
psico <desenvolvedorjg@gmail.com>
2021-01-20 00:08:34 -0300

    [UPT] Removed unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,9 @@
 const express = require("express");
 const { graphqlHTTP } = require('express-graphql');
-// const { makeExecutableSchema } = require("graphql-tools");
 
-const fs = require("fs")
-const path = require("path");
-// const schemaFile = path.join(__dirname, "schema.graphql");
-// const typeDefs = fs.readFileSync(schemaFile, "utf8");
 import schema from './schema';
 
-const providers = {
-    users: []
-};
-
-// const resolvers = {
-//     Query: {
-//         users: () => {
-//             return providers.users;
-//         },
-//         user: (_: any, {id}: any) => {
-//             // @ts-ignore
-//             return providers.users.find(item => item.id === Number(id));
-//         },
-//     },
-//     User: {
-//         id: (user: { id: any; }) => user.id,
-//         name: (user: { name: string; }) => user.name,
-//         repo: (user: { repo: string; }) => user.repo,
-//         age: (user: { age: number; }) => user.age,
-//     },
-//     Mutation: {
-//         // @ts-ignore
-//         createUser(_, { name, repo, age }) {
-//             const user = {
-//                 id: 1,
-//                 name,
-//                 repo,
-//                 age
-//             };
-//
-//             // @ts-ignore
-//             providers.users.push(user);
-//
-//             return user;
-//         }
-//     },
-// };
-
-
-// const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-var app = express();
+const app = express();
 
 app.use(
     "/graphql",
