920098baaedc9a3694e89da12f16b904f869091d
psico <desenvolvedorjg@gmail.com>
2021-01-19 21:03:29 -0300

    [ADD] User resolver implemented

---
+++ src/user/resolvers.ts
@@ -0,0 +1,33 @@
+export const Query = {
+    users: () => {
+        return providers.users;
+    },
+    user: (_: any, {id}: any) => {
+        // @ts-ignore
+        return providers.users.find(item => item.id === Number(id));
+    },
+};
+
+export const Mutation = {
+    // @ts-ignore
+    createUser(_, { name, repo, age }) {
+        const user = {
+            id: 1,
+            name,
+            repo,
+            age
+        };
+
+        // @ts-ignore
+        providers.users.push(user);
+
+        return user;
+    }
+};
+
+export const User = {
+    id: (user: { id: any; }) => user.id,
+    name: (user: { name: string; }) => user.name,
+    repo: (user: { repo: string; }) => user.repo,
+    age: (user: { age: number; }) => user.age,
+};
