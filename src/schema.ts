369b0a6fda96f1a96b8cfde4ede3796cd9fef462
psico <desenvolvedorjg@gmail.com>
2021-01-20 00:07:11 -0300

    [UPT] call base schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,8 +1,10 @@
 import { makeExecutableSchema } from 'graphql-tools';
+import Base from './base';
+import User from './user/schema';
 import resolvers from './resolvers'
 
 export default makeExecutableSchema({
-    typeDefs: [],
+    typeDefs: [Base, User],
     resolvers,
     logger: { log: e => console.log(e) },
 })
