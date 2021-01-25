562cf5a77d82c0f767b2de77df4e930a303d1e8a
psico <desenvolvedorjg@gmail.com>
2021-01-24 22:55:53 -0300

    [UPT] Importing new types

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -3,13 +3,21 @@ import {
     Mutation as UserMutation,
     User
 } from "./user/resolvers";
+import {
+    Query as UserInfoQuery,
+    Mutation as UserInfoMutation,
+    UserInfo
+} from "./userInfo/resolvers";
 
 export default {
     Query: {
         ...UserQuery,
+        ...UserInfoQuery,
     },
     Mutation: {
         ...UserMutation,
+        ...UserInfoMutation,
     },
-    User
+    User,
+    UserInfo
 }
