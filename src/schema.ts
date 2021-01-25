c44b3214598e1d46aabfbedc429b02eccfecdf46
psico <desenvolvedorjg@gmail.com>
2021-01-24 22:55:38 -0300

    [UPT] Importing new types

--- src/schema.ts
+++ src/schema.ts
@@ -1,10 +1,13 @@
 import { makeExecutableSchema } from 'graphql-tools';
 import Base from './base';
 import User from './user/schema';
+import UserInfo from './userInfo/schema';
+import Friend from './friend/schema';
 import resolvers from './resolvers'
 
+
 export default makeExecutableSchema({
-    typeDefs: [Base, User],
+    typeDefs: [Base, User, UserInfo, Friend],
     resolvers,
     logger: { log: e => console.log(e) },
 })
