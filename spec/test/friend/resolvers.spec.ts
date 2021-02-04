3fc7c16c95129840dfd72bfc651fec315d3ae039
psico <desenvolvedorjg@gmail.com>
2021-02-04 07:02:38 -0300

    [UPT] updated name of test

--- spec/test/friend/resolvers.spec.ts
+++ spec/test/friend/resolvers.spec.ts
@@ -1,7 +1,7 @@
 const { Query } = require("../../../src/friend/resolvers");
 
 describe("TEST Friend", () => {
-    it("some thing to test", () => {
+    it("Function friends", () => {
         expect(typeof Query.friends()).toBe("object");
     });
 });
