9ea212f4626615dc6981c2d2e716b02ce6f05042
psico <desenvolvedorjg@gmail.com>
2021-02-09 19:24:36 -0300

    [ADD] Added teste createFriend

--- spec/test/friend/resolvers.spec.ts
+++ spec/test/friend/resolvers.spec.ts
@@ -1,11 +1,18 @@
-const { Query } = require("../../../src/friend/resolvers");
+const { Query, Mutation } = require("../../../src/friend/resolvers");
 
-describe("TEST Friend", () => {
+describe("QUERY Friend", () => {
     it("Function friends()", () => {
         expect(typeof Query.friends()).toBe("object");
     });
 
     it("Function friend()", () => {
         expect(typeof Query.friend("",{id:0})).toBe("undefined");
+    });
+});
+
+describe("MUTATION Friend", () => {
+    it("Function createFriend()", () => {
+        expect(typeof Mutation.createFriend("", { uid: 1, name: "name" }))
+            .toBe("object");
     });
 });
