288fa0586c1bdef8426d2ae1230bc38b6a96624f
psico <desenvolvedorjg@gmail.com>
2021-02-10 21:10:00 -0300

    [UPT] Added assertion name teste

--- spec/test/friend/resolvers.spec.ts
+++ spec/test/friend/resolvers.spec.ts
@@ -25,4 +25,10 @@ describe("MODEL Friend", () => {
         expect(typeof uid).toBe("string");
         expect(uid).toBe("1");
     });
+
+    it("Property name", () => {
+        const name = Friend.name({name: "name"});
+        expect(typeof name).toBe("string");
+        expect(name).toBe("name");
+    });
 });
