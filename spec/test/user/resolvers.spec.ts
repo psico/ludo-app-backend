f7e2adfa59134a689a2f693cf5dec9ce0808803c
psico <desenvolvedorjg@gmail.com>
2021-02-11 18:27:04 -0300

    [ADD] Added test function createUser

--- spec/test/user/resolvers.spec.ts
+++ spec/test/user/resolvers.spec.ts
@@ -1,4 +1,4 @@
-const {Query} = require("../../../src/user/resolvers");
+const {Query, Mutation} = require("../../../src/user/resolvers");
 
 describe("QUERY User", () => {
     it("Function users()", () => {
@@ -7,5 +7,15 @@ describe("QUERY User", () => {
 
     it("Function user()", () => {
         expect(typeof Query.user("", {id: 0})).toBe("undefined");
+    });
+});
+
+describe("MUTATION User", () => {
+    it("Function createUser()", () => {
+        const user = Mutation.createUser("", {name: "name", repo: "repo", age: 1})
+        expect(typeof user).toBe("object");
+        expect(user.name).toBe("name");
+        expect(user.repo).toBe("repo");
+        expect(user.age).toBe(1);
     });
 });
