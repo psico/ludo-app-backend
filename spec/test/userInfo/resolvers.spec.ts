f00af7e96605775bbcb1cb0cb4a34d2748dda33c
psico <desenvolvedorjg@gmail.com>
2021-02-13 23:15:30 -0300

    [ADD] Added test to function userInfo

--- spec/test/userInfo/resolvers.spec.ts
+++ spec/test/userInfo/resolvers.spec.ts
@@ -4,4 +4,8 @@ describe("QUERY UserInfo", () => {
     it("Function usersInfo()", () => {
         expect(typeof Query.usersInfo()).toBe("object");
     });
+
+    it("Function userInfo()", () => {
+        expect(typeof Query.userInfo("", {id: 0})).toBe("undefined");
+    });
 });
