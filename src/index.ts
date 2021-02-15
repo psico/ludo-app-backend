57fa91ee909d725c3c5be357d6955f69a76f8763
psico <desenvolvedorjg@gmail.com>
2021-02-15 16:21:19 -0300

    [ADD] Begin to connect to firebase.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
 const express = require("express");
 const { graphqlHTTP } = require('express-graphql');
 
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
 import schema from './schema';
+
+const firebaseConfig = {
+    apiKey: "API_KEY",
+    authDomain: "PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://PROJECT_ID.firebaseio.com",
+    projectId: "PROJECT_ID",
+    storageBucket: "PROJECT_ID.appspot.com",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID",
+    measurementId: "G-MEASUREMENT_ID",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const app = express();
 
