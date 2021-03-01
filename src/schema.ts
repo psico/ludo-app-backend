8a14511d2825c24253eb565a6d2128d4e42b702c
psico <desenvolvedorjg@gmail.com>
2021-02-28 21:56:07 -0300

    [UPT] Removed unnecessary code.

--- src/schema.ts
+++ src/schema.ts
@@ -1,13 +1,12 @@
 import { makeExecutableSchema } from 'graphql-tools';
 import Base from './base';
-import User from './user/schema';
 import UserInfo from './userInfo/schema';
 import Friend from './friend/schema';
 import resolvers from './resolvers'
 
 
 export default makeExecutableSchema({
-    typeDefs: [Base, User, UserInfo, Friend],
+    typeDefs: [Base, UserInfo, Friend],
     resolvers,
     logger: { log: e => console.log(e) },
 })
