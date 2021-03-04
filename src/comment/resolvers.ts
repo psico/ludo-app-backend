4e2ed6e778db2aa60aa97d967980751e444f3ff4
psico <desenvolvedorjg@gmail.com>
2021-03-03 23:21:09 -0300

    [ADD] Begin to implement comment resolver

---
+++ src/comment/resolvers.ts
@@ -0,0 +1,37 @@
+import { db } from "../index";
+
+export const Query = {
+    comments: async () => {
+        let comments: Array<object> = [];
+
+        // const snapshot = await db.collection("usersInfo").get();
+        // snapshot.forEach((doc: any) => {
+        //     comments.push(doc.data())
+        // });
+
+        return comments;
+    },
+    comment: async (_: any, {uid}: any) => {
+        // const usersInfoRef = db.collection("usersInfo");
+        // const snapshot = await usersInfoRef.where("uid","==", uid).get();
+
+        return snapshot.docs[0].data();
+    },
+};
+
+export const Mutation = {
+    async createComment(_: any, { uid, name, comment }: any) {
+        // const docRef = db.collection('usersInfo').doc();
+
+        return docRef.set({
+            name: name,
+            uid: uid
+        });
+    }
+};
+
+export const Comment = {
+    uid: (comment: { uid: string; }) => comment.uid,
+    name: (comment: { name: string; }) => comment.name,
+    comment: (comment: { comment: string; }) => comment.comment
+};
