7d8e982597da70b47c8b13e89f2d5c866116a70e
psico <desenvolvedorjg@gmail.com>
2021-03-09 18:33:17 -0300

    [UPT] Begint to implement object game

--- src/game/resolvers.ts
+++ src/game/resolvers.ts
@@ -1,34 +1,36 @@
 import { db } from "../index";
 
-export const Query = {
-    games: async () => {
-        let games: Array<object> = [];
+// export const Query = {
+//     games: async () => {
+//         let games: Array<object> = [];
+//
+//         // const snapshot = await db.collection("usersInfo").get();
+//         // snapshot.forEach((doc: any) => {
+//         //     games.push(doc.data())
+//         // });
+//
+//         return games;
+//     },
+//     game: async (_: any, {uid}: any) => {
+//         // const usersInfoRef = db.collection("usersInfo");
+//         // const snapshot = await usersInfoRef.where("uid","==", uid).get();
+//
+//         // return snapshot.docs[0].data();
+//     },
+// };
 
-        // const snapshot = await db.collection("usersInfo").get();
-        // snapshot.forEach((doc: any) => {
-        //     games.push(doc.data())
-        // });
+// export const Mutation = {
+//     async createGame(_: any, { gameMoment }: any) {
+//         // const docRef = db.collection('usersInfo').doc();
+//
+//         // return docRef.set({
+//         //     gameMoment: gameMoment
+//         // });
+//     }
+// };
 
-        return games;
-    },
-    game: async (_: any, {uid}: any) => {
-        // const usersInfoRef = db.collection("usersInfo");
-        // const snapshot = await usersInfoRef.where("uid","==", uid).get();
-
-        // return snapshot.docs[0].data();
-    },
-};
-
-export const Mutation = {
-    async createComment(_: any, { gameMoment }: any) {
-        // const docRef = db.collection('usersInfo').doc();
-
-        // return docRef.set({
-        //     gameMoment: gameMoment
-        // });
-    }
-};
-
-export const Comment = {
-    gameMoment: (game: { gameMoment: string; }) => game.gameMoment
+export const Game = {
+    name: (game: { name: string; }) => game.name,
+    objectId: (game: { objectId: string; }) => game.objectId,
+    yearPublished: (game: { yearPublished: Number; }) => game.yearPublished
 };
