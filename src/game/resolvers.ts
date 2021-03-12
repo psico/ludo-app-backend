b03f25a5d6bcb1e3c3017f62c89eec54535faf70
psico <desenvolvedorjg@gmail.com>
2021-03-11 21:44:41 -0300

    [UPT] Begint to implement resolver game

--- src/game/resolvers.ts
+++ src/game/resolvers.ts
@@ -1,33 +1,19 @@
-import { db } from "../index";
+export const Query = {
+    games: async () => {
+        let games: Array<object> = [];
 
-// export const Query = {
-//     games: async () => {
-//         let games: Array<object> = [];
-//
-//         // const snapshot = await db.collection("usersInfo").get();
-//         // snapshot.forEach((doc: any) => {
-//         //     games.push(doc.data())
-//         // });
-//
-//         return games;
-//     },
-//     game: async (_: any, {uid}: any) => {
-//         // const usersInfoRef = db.collection("usersInfo");
-//         // const snapshot = await usersInfoRef.where("uid","==", uid).get();
-//
-//         // return snapshot.docs[0].data();
-//     },
-// };
+        return games;
+    },
+    game: async (_: any, {uid}: any) => {
 
-// export const Mutation = {
-//     async createGame(_: any, { gameMoment }: any) {
-//         // const docRef = db.collection('usersInfo').doc();
-//
-//         // return docRef.set({
-//         //     gameMoment: gameMoment
-//         // });
-//     }
-// };
+    },
+};
+
+export const Mutation = {
+    async createGame(_: any, { gameMoment }: any) {
+
+    }
+};
 
 export const Game = {
     name: (game: { name: string; }) => game.name,
