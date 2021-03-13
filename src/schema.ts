5556b513f123cc033d069ff1e210c81c085bf61e
psico <desenvolvedorjg@gmail.com>
2021-03-13 19:55:41 -0300

    [UPT] join comment object to main class

--- src/schema.ts
+++ src/schema.ts
@@ -2,13 +2,14 @@ import { makeExecutableSchema } from 'graphql-tools';
 import Base from './base';
 import UserInfo from './userInfo/schema';
 import Friend from './friend/schema';
-import Match from './match/schema';
 import Game from './game/schema';
+import Comment from './comment/schema';
+import Match from './match/schema';
 import resolvers from './resolvers';
 
 
 export default makeExecutableSchema({
-    typeDefs: [Base, UserInfo, Friend, Game, Match],
+    typeDefs: [Base, UserInfo, Friend, Game, Comment, Match],
     resolvers,
     logger: { log: e => console.log(e) },
 })
