847468136990317fec045ee29e21e8aaf272e81b
psico <desenvolvedorjg@gmail.com>
2021-03-23 22:04:32 -0300

    [DEL] Removed unnecessary code

--- src/comment/resolvers.ts
+++ src/comment/resolvers.ts
@@ -1,36 +1,3 @@
-import { db } from "../index";
-
-export const Query = {
-    comments: async () => {
-        let comments: Array<object> = [];
-
-        const snapshot = await db.collection("usersInfo").get();
-        snapshot.forEach((doc: any) => {
-            comments.push(doc.data())
-        });
-
-        return comments;
-    },
-    comment: async (_: any, {uid}: any) => {
-        // const usersInfoRef = db.collection("usersInfo");
-        // const snapshot = await usersInfoRef.where("uid","==", uid).get();
-
-        // return snapshot.docs[0].data();
-    },
-};
-
-export const Mutation = {
-    async createComment(_: any, { uid, name, comment }: any) {
-        // const docRef = db.collection('usersInfo').doc();
-
-        // return docRef.set({
-        //     uid: uid,
-        //     name: name,
-        //     comment: comment
-        // });
-    }
-};
-
 export const Comment = {
     uid: (comment: { uid: string; }) => comment.uid,
     name: (comment: { name: string; }) => comment.name,
--- src/comment/schema.ts
+++ src/comment/schema.ts
@@ -1,11 +1,4 @@
 const Comment = `
-extend type Query {
-    comment(uid: ID!): Comment
-    comments: [Comment]
-}
-extend type Mutation {
-    createComment(uid: ID!, name: String!, comment: String!): Comment
-}
 type Comment {
     uid: ID
     name: String
