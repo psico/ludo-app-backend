0d29fee000097e523abf2669ce51abccf3a13095
psico <desenvolvedorjg@gmail.com>
2021-05-02 23:54:54 -0300

    [UPT] Post body working

--- src/index.ts
+++ src/index.ts
@@ -25,15 +25,36 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
-//Initiating graphQL
-app.post('/login',
-    passport.authenticate('local'),
-    function (req: any, res: any) {
-        // If this function gets called, authentication was successful.
-        // `req.user` contains the authenticated user.
-        res.redirect('/users/' + req.user.username);
-    }
-);
+// Initiating login
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+// app.post('/login',
+//     passport.authenticate('local'),
+//     function (req: any, res: any) {
+//         // If this function gets called, authentication was successful.
+//         // `req.user` contains the authenticated user.
+//         // res.redirect('/users/' + req.user.username);
+//
+//         console.log(req.data);
+//         console.log(req.params);
+//         console.log(req.body);
+//         //JG implements
+//         res.send({
+//             token: 'test12345'
+//         });
+//     }
+// );
+app.use('/login', (req: any, res: any) => {
+    console.log(req.data);
+    console.log(req.params);
+    console.log(req.body);
+    res.send({
+        token: 'test123'
+    });
+});
 
 //Initiating graphQL
 app.use(
