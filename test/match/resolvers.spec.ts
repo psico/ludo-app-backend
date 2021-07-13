d214e521a04ada0046024f8031fdf6fc1e36a7b0
psico <desenvolvedorjg@gmail.com>
2021-07-13 18:34:48 -0300

    [ADD] Begin to implements test match

---
+++ test/match/resolvers.spec.ts
@@ -0,0 +1,34 @@
+// const {Query, Mutation, Friend} = require("../../src/friend/resolvers");
+//
+describe("QUERY Match", () => {
+    it("Function matches()", () => {
+//         expect(typeof Query.friends()).toBe("object");
+    });
+//
+//     it("Function friend()", () => {
+//         expect(typeof Query.friend("", {id: 0})).toBe("undefined");
+//     });
+});
+//
+// describe("MUTATION Friend", () => {
+//     it("Function createFriend()", () => {
+//         const friend = Mutation.createFriend("", {uid: 1, name: "name"})
+//         expect(typeof friend).toBe("object");
+//         expect(friend.uid).toBe(1);
+//         expect(friend.name).toBe("name");
+//     });
+// });
+//
+// describe("MODEL Friend", () => {
+//     it("Property uid", () => {
+//         const uid = Friend.uid({uid: "1"});
+//         expect(typeof uid).toBe("string");
+//         expect(uid).toBe("1");
+//     });
+//
+//     it("Property name", () => {
+//         const name = Friend.name({name: "name"});
+//         expect(typeof name).toBe("string");
+//         expect(name).toBe("name");
+//     });
+// });
