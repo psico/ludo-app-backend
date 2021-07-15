61201fbf7ae2d14efd4efe8d8ac495ac84924b98
psico <desenvolvedorjg@gmail.com>
2021-07-15 13:52:22 -0300

    [UPT] Trying to use context

--- src/match/resolvers.ts
+++ src/match/resolvers.ts
@@ -1,12 +1,11 @@
-import { db } from '../index';
 import firebase from 'firebase';
 import axios from 'axios';
 
 export const Query = {
-  matches: async () => {
+  matches: async (_: any, __: any, context:any) => {
     const matches: Array<object> = [];
 
-    const snapshot = await db.collection('matches').orderBy('createdAt', 'desc').get();
+    const snapshot = await context.collection('matches').orderBy('createdAt', 'desc').get();
 
     snapshot.forEach((doc: any) => {
       matches.push({
@@ -17,18 +16,18 @@ export const Query = {
 
     return matches;
   },
-  match: async (_: any, { idDoc }: any) => {
-    const snapshot = await db.collection('matches').doc(idDoc).get();
+  match: async (_: any, { idDoc }: any, context:any) => {
+    const snapshot = await context.collection('matches').doc(idDoc).get();
 
     return snapshot.data();
   }
 };
 
 export const Mutation = {
-  async createMatch (_: any, { MatchInput }: any) {
+  async createMatch (_: any, { MatchInput }: any, context:any) {
     const request:any = await axios.get(`https://api.boardgameatlas.com/api/search?ids=${MatchInput.gameObjectId}&pretty=true&client_id=fceBG35WbJ`);
     const userData: firebase.User | null = await firebase.auth().currentUser;
-    const docRef = db.collection('matches').doc();
+    const docRef = context.collection('matches').doc();
 
     let gameObject = null;
     if (request.data) {
@@ -67,9 +66,9 @@ export const Mutation = {
     };
   },
 
-  async addComment (_: any, { CommentInput }: any) {
+  async addComment (_: any, { CommentInput }: any, context:any) {
     const userData: any = await firebase.auth().currentUser;
-    const docRef = db.collection('matches').doc(CommentInput.idDoc);
+    const docRef = context.collection('matches').doc(CommentInput.idDoc);
     const snapshot = await docRef.get();
     const objMatch = snapshot.data();
 
