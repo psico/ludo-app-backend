fc3227b3fc6d53527ae9338e82712701e37e3b19
psico <desenvolvedorjg@gmail.com>
2021-07-17 19:14:48 -0300

    [UPT] Updated with eslint format.

--- src/userInfo/resolvers.ts
+++ src/userInfo/resolvers.ts
@@ -1,39 +1,39 @@
-import {db} from "../index";
+import { db } from '../index';
 
 export const Query = {
-    usersInfo: async () => {
-        let usersInfo: Array<object> = [];
+  usersInfo: async () => {
+    const usersInfo: Array<object> = [];
 
-        const snapshot = await db.collection("usersInfo").get();
-        snapshot.forEach((doc: any) => {
-            usersInfo.push(doc.data())
-        });
+    const snapshot = await db.collection('usersInfo').get();
+    snapshot.forEach((doc: any) => {
+      usersInfo.push(doc.data());
+    });
 
-        return usersInfo;
-    },
-    userInfo: async (_: any, {uid}: any) => {
-        const usersInfoRef = db.collection("usersInfo");
-        const snapshot = await usersInfoRef.where("uid","==", uid).get();
+    return usersInfo;
+  },
+  userInfo: async (_: any, { uid }: any) => {
+    const usersInfoRef = db.collection('usersInfo');
+    const snapshot = await usersInfoRef.where('uid', '==', uid).get();
 
-        return snapshot.docs[0].data();
-    },
+    return snapshot.docs[0].data();
+  }
 };
 
 export const Mutation = {
-    createUserInfo(_: any, { uid, name }: any) {
-        const docRef = db.collection('usersInfo').doc();
-        //@TODO to do a check to validate  that uid exists
-        docRef.set({
-            name,
-            uid,
-        });
+  createUserInfo (_: any, { uid, name }: any) {
+    const docRef = db.collection('usersInfo').doc();
+    // @TODO to do a check to validate  that uid exists
+    docRef.set({
+      name,
+      uid
+    });
 
-        return { uid, name };
-    }
+    return { uid, name };
+  }
 };
 
 export const UserInfo = {
-    uid: (userInfo: { uid: any; }) => userInfo.uid,
-    name: (userInfo: { name: string; }) => userInfo.name,
-    friends: (userInfo: { friends: Array<any>; }) => userInfo.friends
+  uid: (userInfo: { uid: any; }) => userInfo.uid,
+  name: (userInfo: { name: string; }) => userInfo.name,
+  friends: (userInfo: { friends: Array<any>; }) => userInfo.friends
 };
