ca37c46de6ba2e96a2ce0b9fc66d18fe7475ac8f
psico <desenvolvedorjg@gmail.com>
2021-07-20 22:35:36 -0300

    [UPT] Better code to mock

--- test/match/resolvers.spec.ts
+++ test/match/resolvers.spec.ts
@@ -1,49 +1,38 @@
 import { Query } from '../../src/match/resolvers';
 import firebase from 'firebase';
-import Timestamp = firebase.firestore.Timestamp;
 
-// const queryStub = collection => {
-//   if (q == 'SELECT * from tweets') {
-//     return Promise.resolve({
-//       rows: [
-//         { id: 1, body: 'hello' },
-//         { id: 2, body: 'world' }
-//       ]
-//     });
-//   }
-// };
+const mockData = [{
+  // eslint-disable-next-line no-undef
+  data: jest.fn(() => ({
+    createdAt: firebase.firestore.Timestamp.now(),
+    gameMoment: 'play-now',
+    game: {
+      name: 'Test Game',
+      description: 'Only for test',
+      yearPublished: 2021,
+      objectId: 'Teste'
+    },
+    uid: 'testTestTestTestTestTestTest'
+  }))
+}];
+
+const context = {
+  // eslint-disable-next-line no-undef
+  collection: jest.fn(x => ({
+    // eslint-disable-next-line no-undef
+    orderBy: jest.fn(x => ({
+      // eslint-disable-next-line no-undef
+      get: jest.fn(() => mockData)
+    }))
+  }))
+};
 
 // eslint-disable-next-line no-undef
 describe('QUERY Match', () => {
   // eslint-disable-next-line no-undef
   it('Function matches()', async () => {
-    const mockData = [{
-      // eslint-disable-next-line no-undef
-      data: jest.fn(() => ({
-        createdAt: Timestamp.now(),
-        gameMoment: 'play-now',
-        game: {
-          name: 'Ab die Post!',
-          description: '',
-          yearPublished: 1996,
-          objectId: 'H3sDuNTtK0'
-        },
-        uid: '0IhNFZFa7QMwBY6yZT8l24L1AX32'
-      }))
-    }];
-
-    const context = {
-      // eslint-disable-next-line no-undef
-      collection: jest.fn(x => ({
-        // eslint-disable-next-line no-undef
-        orderBy: jest.fn(x => ({
-          // eslint-disable-next-line no-undef
-          get: jest.fn(() => mockData)
-        }))
-      }))
-    };
     const data = await Query.matches(null, null, context);
-    console.log('data => ', data);
+
     //         expect(typeof Query.friends()).toBe("object");
   });
 //
