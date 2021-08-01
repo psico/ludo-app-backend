84a451799c355503a8de16d20edffd2f9109a10e
psico <desenvolvedorjg@gmail.com>
2021-07-31 21:17:27 -0300

    [UPT] starting test function usersInfo()

--- test/userInfo/resolvers.spec.ts
+++ test/userInfo/resolvers.spec.ts
@@ -1,45 +1,30 @@
 const { Query } = require('../../src/userInfo/resolvers');
 
-const db = {
+const mockData = {
+  data: {
+    usersInfo: [
+      {
+        uid: 'uid-test-uid-test-uid-test-u',
+        name: 'Test',
+        friends: [
+          {
+            uid: 'friend-uid-test-friend-uid-t',
+            name: 'Desenvolvedork JG'
+          }
+        ]
+      }
+    ]
+  }
+};
 
+const db = {
+  collection: jest.fn(() => ({
+    get: jest.fn(() => (mockData))
+  }))
 };
 
 describe('QUERY UserInfo', () => {
   it('Function usersInfo()', () => {
     Query.usersInfo(null, null, db);
-    //         expect(typeof Query.usersInfo()).toBe("object");
   });
-//
-//     it("Function userInfo()", () => {
-//         expect(typeof Query.userInfo("", {id: 0})).toBe("undefined");
-//     });
 });
-//
-// describe("MUTATION UserInfo", () => {
-//     it("Function createUserInfo()", () => {
-//         const userInfo = Mutation.createUserInfo("", {uid: 1, name: "name"})
-//         expect(typeof userInfo).toBe("object");
-//         expect(userInfo.uid).toBe(1);
-//         expect(userInfo.name).toBe("name");
-//     });
-// });
-//
-// describe("MODEL UserInfo", () => {
-//     it("Property uid", () => {
-//         const uid = UserInfo.uid({uid: "1"});
-//         expect(typeof uid).toBe("string");
-//         expect(uid).toBe("1");
-//     });
-//
-//     it("Property name", () => {
-//         const name = UserInfo.name({name: "name"});
-//         expect(typeof name).toBe("string");
-//         expect(name).toBe("name");
-//     });
-//
-//     // it("Property friends", () => {
-//     //     const friends = UserInfo.friends({friends: []});
-//     //     expect(typeof friends).toBe("object");
-//     //     expect(friends).toBe();
-//     // });
-// });
