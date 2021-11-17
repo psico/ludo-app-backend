dac7a9f73ce79c6bf616d89ade53227b75082e00
psico <desenvolvedorjg@gmail.com>
2021-11-16 23:07:34 -0300

    [UPT] Beging to implement to follow friend

--- src/friend/resolvers.ts
+++ src/friend/resolvers.ts
@@ -1,38 +1,43 @@
-import { db } from "../index";
+import { db } from '../index';
 
 export const Query = {
-    friends: async () => {
-        let friends: Array<object> = [];
+  friends: async () => {
+    const friends: Array<object> = [];
 
-        const snapshot = await db.collection("usersInfo").get();
-        snapshot.forEach((doc: any) => {
-            friends.push(doc.data())
-        });
+    const snapshot = await db.collection('usersInfo').get();
+    snapshot.forEach((doc: any) => {
+      friends.push(doc.data());
+    });
 
-        return friends;
-    },
-    friend: async (_: any, {uid}: any) => {
-        const usersInfoRef = db.collection("usersInfo");
-        const snapshot = await usersInfoRef.where("uid","==", uid).get();
+    return friends;
+  },
 
-        return snapshot.docs[0].data();
-    },
+  friend: async (_: any, { uid }: any) => {
+    const usersInfoRef = db.collection('usersInfo');
+    const snapshot = await usersInfoRef.where('uid', '==', uid).get();
+
+    return snapshot.docs[0].data();
+  }
 };
 
 export const Mutation = {
-    createFriend(_: any, { uid, name }: any) {
-        const docRef = db.collection('usersInfo').doc();
+  createFriend (_: any, { uid, name }: any) {
+    const docRef = db.collection('usersInfo').doc();
 
-        docRef.set({
-            name: name,
-            uid: uid
-        });
+    docRef.set({
+      name: name,
+      uid: uid
+    });
 
-        return { uid, name };
-    }
+    return { uid, name };
+  },
+
+  follow (_: any, { uid }: any) {
+    return {};
+  }
 };
 
 export const Friend = {
-    uid: (friend: { uid: string; }) => friend.uid,
-    name: (friend: { name: string; }) => friend.name
+  uid: (friend: { uid: string; }) => friend.uid,
+  name: (friend: { name: string; }) => friend.name
 };
