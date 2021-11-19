4b802956899ce87217d903e4f4dcfccb8e5d3e72
psico <desenvolvedorjg@gmail.com>
2021-11-18 21:26:37 -0300

    [UPT] Beging to implement to follow friend

--- src/friend/schema.ts
+++ src/friend/schema.ts
@@ -5,7 +5,7 @@ extend type Query {
 }
 extend type Mutation {
     createFriend(uid: ID!, name: String!): Friend
-    follow(uid: ID!): Friend
+    follow(uid: ID!, followUid: ID!): Friend
 }
 type Friend {
     uid: ID
